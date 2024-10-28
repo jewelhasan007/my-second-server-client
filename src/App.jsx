f04404b1d0a34736f641e2b8e818997c350d2b8c
Jewel Hasan <jewel.hasan@cg-bd.com>
2024-10-28 09:33:15 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react';
+import './App.css'
+
+function App() {
+const [profile, setProfile] = useState([])
+
+  useEffect(()=>{
+    fetch('http://localhost:5000/users')
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      setProfile(data)
+    } )
+  },[])
+
+  const handleNewUser = event =>{
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const users = {name, email}
+    console.log(users)
+  }
+
+
+  return (
+    <>
+      <h1 className='text-red-400'>User Management</h1>
+      {
+      profile.map(profile => <li>{profile.name}:{profile.email}:{profile.id}</li>)
+     }
+     <form onSubmit={handleNewUser}>
+     <div>
+       <input type="name" name='name' placeholder="name" className="input input-bordered" required />
+      </div>
+     <div>
+       <input type="email" name='email' placeholder="email" className="input input-bordered" required />
+      </div>
+      <button type="submit" className='border'>Submit</button>
+     </form>
+   
+    
+    
+    </>
+  )
+}
+
+export default App
